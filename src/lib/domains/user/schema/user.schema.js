a5265cae8d9bcba2443b5e215428147dca6e8ca0
donutsleepy <katze3621@gmail.com>
2025-12-02 16:32:45 -0500

    Refactor password hashing and remove middleware
    Password hashing is now handled in the user schema pre-save hook, removing redundant hashing from customer and employee controllers. Increased bcrypt salt rounds to 12 for improved security. Deleted unused middleware.ts file. Updated bcryptjs and validator dependencies.

--- src/lib/domains/user/schema/user.schema.js
+++ src/lib/domains/user/schema/user.schema.js
@@ -32,8 +32,7 @@ const UserBaseSchema = new Schema(
 
 UserBaseSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
+  this.password = await bcrypt.hash(this.password, 12);
   next();
 });
 
